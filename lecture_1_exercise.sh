89206c9ae81ec1a6d3b2a8ab8fb2ac98467edf84
HARLAN XU <73050875+Harlan2001@users.noreply.github.com>
2023-04-10 20:18:45 +0800

    Update lecture_1_exercise.sh

--- lecture_1_exercise.sh
+++ lecture_1_exercise.sh
@@ -1,25 +1,45 @@
-#Create a new directory called missing under /tmp.
+#2.Create a new directory called missing under /tmp.
+mkdir tmp  # I don't have tmp folder in my virtural machine, so i just make one
 cd tmp
 mkdir missing
 
-#Look up the touch program. The man program is your friend.
+#3.Look up the touch program. The man program is your friend.
+man touch
 
-#Use touch to create a new file called semester in missing.
+#4.Use touch to create a new file called semester in missing.
+touch semester
 
-#Write the following into that file, one line at a time:
+#5.Write the following into that file, one line at a time:
 #!/bin/sh
 #curl --head --silent https://missing.csail.mit.edu
-#The first line might be tricky to get working. It’s helpful to know that # starts a comment in Bash, and ! has a special meaning even within double-quoted (") strings. Bash treats single-quoted strings (') differently: they will do the trick in this case. 
-#See the Bash quoting manual page for more information.
+#The first line might be tricky to get working. It’s helpful to know that # starts a comment in Bash, and ! has a special meaning even within double-quoted (") strings. Bash treats single-quoted strings (') differently: they will do the trick in this case. See the Bash quoting manual page for more information.
+ehco '#!/bin/sh' > semester
+echo curl --head --silent https://missing.csail.mit.edu >> semester # here is >>, use > will just overwrite the file
+cat semester
+# #!/bin/sh
+# curl --head --silent https://missing.csail.mit.edu
 
-#Try to execute the file, i.e. type the path to the script (./semester) into your shell and press enter. Understand why it doesn’t work by consulting the output of ls (hint: look at the permission bits of the file).
+#6.Try to execute the file, i.e. type the path to the script (./semester) into your shell and press enter. Understand why it doesn’t work by consulting the output of ls (hint: look at the permission bits of the file).
+./semester
+#bash: ./semester: 权限不够
+ls -l
+#总用量 4
+#drwxrwxr-x. 2 harlan harlan  6 4月  10 19:52 missing
+#-rw-rw-r--. 1 harlan harlan 61 4月  10 20:07 semester
 
-#Run the command by explicitly starting the sh interpreter, and giving it the file semester as the first argument, i.e. sh semester. Why does this work, while ./semester didn’t?
+#7.Look up the chmod program (e.g. use man chmod).
+man chmod
 
-#Look up the chmod program (e.g. use man chmod).
+#8.Use chmod to make it possible to run the command ./semester rather than having to type sh semester. How does your shell know that the file is supposed to be interpreted using sh? See this page on the shebang line for more information.
+chmod 777 semester
+ls -l
+#总用量 4
+#drwxrwxr-x. 2 harlan harlan  6 4月  10 19:52 missing
+#-rwxrwxrwx. 1 harlan harlan 61 4月  10 20:07 semester
 
-#Use chmod to make it possible to run the command ./semester rather than having to type sh semester. How does your shell know that the file is supposed to be interpreted using sh? See this page on the shebang line for more information.
 
-#Use | and > to write the “last modified” date output by semester into a file called last-modified.txt in your home directory.
+#9.Use | and > to write the “last modified” date output by semester into a file called last-modified.txt in your home directory.
+./semester | grep last-modified > ~/last-modified.txt
 
-#Write a command that reads out your laptop battery’s power level or your desktop machine’s CPU temperature from /sys. Note: if you’re a macOS user, your OS doesn’t have sysfs, so you can skip this exercise.
+#10.Write a command that reads out your laptop battery’s power level or your desktop machine’s CPU temperature from /sys. Note: if you’re a macOS user, your OS doesn’t have sysfs, so you can skip this exercise.
+#I use cent os 7, and no such file in my virtual machine, sorry
